31d779d966ee0269f1b8ae4ccbfbbb3cdfe8c5aa
Wasiur Rahman <wrahman0@gmail.com>
2014-11-23 11:42:27 -0500

    Init

---
+++ cas/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cas::Application.config.secret_key_base = '055985ad3a6e6d3c7e982d575262da9e83d449027c1c7b39022a68738f464bb48f11223162e760780b95abc448de7363274682ccab939c8340d4075a98216e2b'
---
+++ cas/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cas::Application.config.session_store :cookie_store, key: '_cas_session'
